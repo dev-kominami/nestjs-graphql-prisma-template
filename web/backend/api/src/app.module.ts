ad3d1724308f41e87d9a0ac361bd1665317a666d
Takashi Kominami <dev.kominami@gmail.com>
2023-01-02 20:11:42 +0900

    refactor: 💡 不要ファイル削除

--- web/backend/api/src/app.module.ts
+++ web/backend/api/src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-// import { PbEnv } from '@config/environments/pb-env.service';
 import { UsersModule } from './components/users/users.module';
-import * as path from 'path';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@prisma-module/prisma.module';
 import { EnvModule } from './config/environments/env.module';
 import { EnvService } from './config/environments/env.service';
@@ -23,7 +17,5 @@ import { EnvService } from './config/environments/env.service';
     UsersModule,
     EnvModule,
   ],
-  // controllers: [AppController],
-  // providers: [PrismaService],
 })
 export class AppModule {}
